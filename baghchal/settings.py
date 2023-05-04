27cb30d758b0928768225fd430d09c46d346bb71
kamal0620040 <neupane0620040@outlook.com>
2023-05-05 00:08:29 +0545

    Registering User

--- baghchal/settings.py
+++ baghchal/settings.py
@@ -137,3 +137,9 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer'
+    }
+}
