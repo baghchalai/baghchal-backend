cad0a4c274d266b54b1e438df1458bc187874d7c
kamal0620040 <neupane0620040@outlook.com>
2023-05-08 20:04:06 +0545

    Working with GameConsumer | writeen logic that makes the game start when two user are present in a group

--- baghchal/asgi.py
+++ baghchal/asgi.py
@@ -11,6 +11,18 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baghchal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import game.routing
+
+application = ProtocolTypeRouter({
+    'http': django_application,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            game.routing.websocket_urlpatterns
+        )
+    ),
+})
--- game/consumers.py
+++ game/consumers.py
@@ -1,16 +1,52 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from urllib.parse import parse_qs
 
 class GameRoomConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.players = {}
+
+    async def connect(self): 
         self.room_name = self.scope['url_route']['kwargs']['room_name']
+        query_params = parse_qs(self.scope['query_string'].decode())
+        self.username = query_params.get('username', [None])[0]
+        print(query_params.get('username', [None])[0])
         self.room_group_name = 'chat_%s' % self.room_name
 
         await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
 
+        print("---------------")
+        print(self.channel_layer.groups.get('baghchal',{}))
+        print(len(self.channel_layer.groups.get(self.room_group_name, {}).items()))
+
         await self.accept()
+
+        self.players[self.channel_name] = self.username
+        print(self.players)
+        # Get all the channel names in the group
+        # players = self.channel_layer.group_channels(self.room_group_name)
+        # print(players)
+        # Retrieve the usernames of the players from the channel names
+        # print([channel.name.split('.')[-1] for channel in players])
+        # await self.accept({'another_player': self.username})
+
+        # Check if there are two players in the room
+        count = self.channel_layer.groups.get(self.room_group_name, {}).items()
+        if len(count) > 3:
+            # await self.send(text_data=json.dumps(message))
+            self.playing_as = 'B'
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'remove_onboarding',
+                    'onboarding_state_value': False,
+                }
+            )
+
+    
 
         # await self.channel_layer.group_send(
         #     self.room_group_name,
@@ -25,6 +61,14 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
     #     await self.send(text_data = json.dumps({
     #         'tester': tester,
     #     }))
+    
+    async def remove_onboarding(self, event):
+        typ = event['type']
+        value = event['onboarding_state_value']
+        await self.send(text_data = json.dumps({
+            'type': typ,
+            'value': value,
+        }))
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name,self.channel_name)
@@ -33,7 +77,7 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         username = text_data_json['username']
-
+        
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -44,10 +88,12 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
         )
 
     async def gameroom_message(self, event):
+        typ = event['type']
         message = event['message']
         username = event['username']
 
         await self.send(text_data=json.dumps({
+            'type': typ,
             'message': message,
             'username': username,
         }))
