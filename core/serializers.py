9f67a89e4eb2dd0aef307c8798c6b31f82ce41c9
kamal0620040 <neupane0620040@outlook.com>
2023-05-27 16:26:08 +0545

    Updated UserSerializer and added profile_present field

--- core/serializers.py
+++ core/serializers.py
@@ -1,9 +1,20 @@
 from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer
+from rest_framework import serializers
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name' ]
 
 class UserSerializer(BaseUserSerializer):
+    profile_present = serializers.SerializerMethodField()
     class Meta(BaseUserSerializer.Meta):
-        fields=['id','username','email','first_name','last_name']
+        fields=['id','username','email','first_name','last_name','profile_present']
+    
+    def get_profile_present(self, obj):
+        try:
+            if obj.player:
+                return True
+            else:
+                return False
+        except:
+            return False
